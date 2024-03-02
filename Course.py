2d347e298ca6e91a1d1974b8238ebdaf3290079b
JohnRuanee <johnruanee@gmail.com>
2024-03-02 10:19:52 -0500

    Created Course Object

---
+++ Course.py
@@ -0,0 +1,12 @@
+class Course:
+
+    def __init__(self, course_code, course_num, section_num, days, start_time, end_time, instructor, traditional):
+        self.course_code = course_code
+        self.course_num = course_num
+        self.section_num = section_num
+        self.days = days
+        self.start_time = start_time
+        self.end_time = end_time
+        self.instructor = instructor
+        self.traditional = traditional
+
