25c263fe774e0e6b5235decabe9da2c255e2906d
JohnRuanee <johnruanee@gmail.com>
2024-03-02 16:26:34 -0500

    did a bit

--- Algorithm.py
+++ Algorithm.py
@@ -1,14 +1,26 @@
 import json
 import Course
+import ratemyprofessor
 
-test_input = ('{'
-              '"requested_courses": ["CPSC 1010", "CPSC 1020", "CPSC 2120"],'
+test_input = (r'{"requested_courses": ["TEST 1010", "TEST 1020", "TEST 1030", "TEST 1040", "TEST 1050"],'
               '"traditional": "True",'
               '"rate": "False",'
-              '"start": "11:00",'
-              '"end": "5:00",'
-              '"num": "9"'
+              '"start": "9:00AM",'
+              '"end": "6:00PM",'
+              '"num": "5"'
               '}')
+
+test_courses = []
+
+def test():
+    for i in range(3):
+        test_courses.append(Course.Course('TEST', 1000 + 10 * (i + 1), 1, 'MWF', f'{9 + i}:00AM', f'{9 + i}:45AM', 'Brice Masek', True))
+    test_courses.append(Course.Course('TEST', 1000 + 10 * (4), 1, 'MWF', f'12:00PM', f'12:45PM', 'Brice Masek', True))
+
+    test_courses.append(
+        Course.Course('TEST', 1000 + 10 * (5), 1, 'MWF', f'{1}:00PM', f'{1}:45PM', 'Brice Masek', True))
+
+
 def parse_input(input):
     req = json.loads(input)
     requested_courses = req["requested_courses"]
@@ -29,7 +41,7 @@ def get_possible_courses(input, courses):
         for course in courses:
             rc = requested_course.split(' ')
 
-            if rc[0] == course.course_code and rc[1] == course.course_num:
+            if rc[0] == course.course_code and int(rc[1]) == course.course_num:
                 possible_courses.append(course)
 
     return possible_courses
@@ -41,40 +53,112 @@ def create_combos(input, courses):
 
     combos = []
 
-    for i in range(len(possible_courses[num - 2:])):
+    for i in range(len(possible_courses[:-1])):
         f_course = possible_courses[i]
-        for s_course in possible_courses[i:]:
+        for s_course in possible_courses[i + 1:]:
             combos.append([f_course, s_course])
 
-    for n in range(num):
-        for i in range(len(possible_courses[num - n])):
-            f_course = possible_courses[i]
-            for s_course in combos:
-                s_course.append(f_course)
+    temp_combos = []
 
-    for combo in combos:
-        c = combo[0]
-        for i in combo[1:]:
-            if c.course_code == i.course_code and c.course_num == i.course_num:
+    for i in range(num - 2):
+        for course in possible_courses:
+            for combo in combos:
+                new_combo = combo.copy()
+                new_combo.append(course)
+                temp_combos.append(new_combo)
+        combos = temp_combos
+        temp_combos = []
+
+    # for n in range(num - 2):
+    #     for i in range(len(possible_courses)):
+    #         f_course = possible_courses[i]
+    #         for s_course in combos:
+    #             s_course.append(f_course)
+    i = 0
+    while i < len(combos):
+        combo = combos[i]
+        combo.sort(key=lambda x: x.course_num)
+        combos, i = find_duplicates(combo, combos, i)
+
+    i, k = 0, 0
+    while i < len(combos) - 1:
+        while k < len(combos) - 1:
+            for n in range(len(combos[0])):
+                    if combos[i][n] != combos[k][n] or k == n:
+                        i += 1
+                        k += 1
+                        break
+            combos.remove(combos[k])
+
+
+
+
+    return combos, start, end
+
+
+def find_duplicates(combo, combos, num):
+    for n in range(len(combo)):
+        c = combo[n]
+        for k in range(len(combo)):
+            i = combo[k]
+            if c.course_code == i.course_code and c.course_num == i.course_num and n != k :
                 combos.remove(combo)
+                return combos, num
             days = {'M': 0, 'T': 0, 'W': 0, 'R': 0, 'F': 0}
             for day in days.keys():
-                if c.days.contains(day) and i.days.contains(day):
-                    if (c.convert_start() > i.convert_end() or c.convert_end > i.convert_start) and c.convert_start < i.convert_start:
+                if day in c.days and day in i.days:
+                    if (
+                            c.convert_start() > i.convert_end() or c.convert_end() > i.convert_start()) and c.convert_start() < i.convert_start():
                         combos.remove(combo)
-                    if (i.convert_start() > c.convert_end() or i.convert_end > c.convert_start) and i.convert_start < c.convert_start:
+                        return combos, num
+                    if (
+                            i.convert_start() > c.convert_end() or i.convert_end() > c.convert_start()) and i.convert_start() < c.convert_start():
                         combos.remove(combo)
+                        return combos, num
+    return combos, num + 1
 
-    return combos
-
-def create_schedules(combos, start, end):
+def create_schedules(input, courses):
+    combos, start, end = create_combos(input, courses)
     schedules = []
     for combo in combos:
         s = Course.Schedule(combo, start, end)
         schedules.append(s)
-def best_schedule(schedules):
+    return schedules
+
+
+def best_schedule(input, courses):
+    schedules = create_schedules(input, courses)
     best = schedules[0]
     for s in schedules:
         if s.score < best.score:
             best = s
     return best
+
+
+def get_best_schedule(input, courses):
+    return best_schedule(input, courses)
+
+
+def create_courses(course_json):
+    req = json.loads(course_json)
+    courses = []
+    for course in req:
+        course_code = req["course_code"]
+        course_num = int(req["course_num"])
+        section_num = int(req["section_num"])
+        days = req["days"]
+        start = req["start_time"]
+        end = req["end_time"]
+        instructor = req["instructor"]
+        traditional = bool(req["traditional"])
+
+        courses.append(Course.Course(course_code,course_num,section_num,days,start,end,instructor,traditional))
+
+    return courses
+
+
+if __name__ == '__main__':
+    test()
+    best = get_best_schedule(test_input, test_courses)
+    for i in best.courses:
+        print(f'{i.course_code} {i.course_num}')
--- Course.py
+++ Course.py
@@ -1,3 +1,5 @@
+import ratemyprofessor
+
 class Course:
 
     def __init__(self, course_code, course_num, section_num, days, start_time, end_time, instructor, traditional):
@@ -19,12 +21,13 @@ class Course:
 
 def convert_time(time_string):
     time = 0
-    if time_string[:-2] == 'AM':
+
+    if time_string[-2:] == 'PM' and time_string[:2] != '12':
         time += 1200
 
     time += (int(time_string[-4:-2]) / 60) * 100
 
-    time += int(time_string.split()[0]) * 100
+    time += int(time_string.split(':')[0]) * 100
 
     return time
 
@@ -32,10 +35,11 @@ def convert_time(time_string):
 class Schedule:
     def __init__(self, courses, start_time, end_time):
         self.courses = courses
+        self.s_start_time = start_time
+        self.s_end_time = end_time
         self.score = self.calculate_score()
         self.credits = 0
-        self.start_time = start_time
-        self.end_time = end_time
+
 
     def calculate_score(self):
         days = {'M' : 0, 'T': 0, 'W' : 0, 'R' : 0, 'F' : 0}
@@ -43,25 +47,36 @@ class Schedule:
 
         for course in self.courses:
             for day in days.keys():
-                days_courses[day].append(course.convert_start())
-                days_courses[day].append(course.convert_end())
+                if day in course.days:
+                    days_courses[day].append(course.convert_start())
+                    days_courses[day].append(course.convert_end())
 
         for day in days.keys():
             days_courses[day] = sorted(days_courses[day])
             times = days_courses[day]
+            if times:
+                start = convert_time(self.s_start_time) - times[0]
+                if start > 0:
+                    days[day] += start
 
-            start = self.start_time - times[0]
-            if start > 0:
-                days[day] += start
+                end = times[-1] - convert_time(self.s_end_time)
+                if end > 0:
+                    days[day] += end
 
-            end = times[:-1] - self.end_time
-            if end > 0:
-                days[day] += end
-
-            for i in range(len(times[2:])):
-                days[day] += times[i] - times[i - 1]
+                for i in range(len(times[2:])):
+                    days[day] += times[i] - times[i - 1]
         score = 0
         for day in days.keys():
             score += days[day]
 
-        return score
+        school = ratemyprofessor.get_school_by_name("Clemson University")
+        rating =  ratemyprofessor.get_professor_by_school_and_name(school, "Kittelstad").rating
+
+        if rating:
+            rating = abs(rating - 5)
+        else:
+            rating = 2
+
+        score += 50 * rating
+
+        return abs(score)
