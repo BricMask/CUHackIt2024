8452aa1cc2733d9144bbc4cfa86fa3616302c1a8
BricMask <bmasek@clemson.edu>
2024-03-02 22:40:31 -0500

    Revert "Revert "FE: DashBoard""
    This reverts commit a2197118616808fd4ec7696524e5e38b35bb715b.

--- cuhackit_ui/src/Dashboard.jsx
+++ cuhackit_ui/src/Dashboard.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function Dashboard() {
-    return (
-      <div className="container-fluid min-vh-100 mw-100">
-        <h2>Dashboard</h2>
-        {/* Your content here */}
-      </div>
-    );
-  }
-  
-  export default Dashboard;
-  
-  
+  return (
+    <div className="container-fluid min-vh-100 mw-100">
+      <h2>Required Classes:</h2>
+      <form>
+          <textarea name="classes" id="classes" cols="30" rows="4"></textarea><br></br> 
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+
+    
+  );
+}
+export default Dashboard;
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+
+//   const data = new FormData(event.target);
+
+//   var value = Object.fromEntries(data.entries());
+
+//   value.topics = data.getAll("topics");
+
+// }
+
+// const form = document.querySelector("form");
+// form.addEventListener("submit", handleSubmit);
