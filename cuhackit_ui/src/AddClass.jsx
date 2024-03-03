18e41db1f76e00e1425008ef11fad368c40d312f
evkes <evkessler2002@gmail.com>
2024-03-02 23:46:43 -0500

    reac

--- cuhackit_ui/src/AddClass.jsx
+++ cuhackit_ui/src/AddClass.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddClass() {
-    return (
-        <div className="container-fluid min-vh-100 mw-100">
-          <h2>Add Class</h2>
-          {/* Your content here */}
+
+  const [response, setResponse] = useState(null);
+    
+  const fetchData = () => {
+    console.log('Fetching data...'); // Added log statement
+    fetch('http://localhost:5000/process', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        requested_courses: ["CPSC 3120", "CPSC 1011", "GEOL 1010", "CPSC 2310", "CPSC 2120"],
+        traditional: "True",
+        rate: "False",
+        start: "9:00AM",
+        end: "6:00PM",
+        num: "5"
+      })
+    })
+    .then(response => {
+      console.log('Received response from server'); // Added log statement
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Setting response data in state'); // Added log statement
+      setResponse(data);
+    })
+    .catch(error => {
+      console.error('There was a problem with your fetch operation:', error);
+    });
+  };
+  
+  return (
+    <div className="container-fluid min-vh-100 mw-100">
+      <button onClick={fetchData}>Fetch Data</button>
+      {response && (
+        <div>
+          <h2>Response:</h2>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
         </div>
-      );
+      )}
+    </div>
+  );
 }
 
 export default AddClass;
